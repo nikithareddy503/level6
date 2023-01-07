302f21f19a381de07e858be038d4061f9ebd939a
nikithareddy503 <nikithareddychinni@gmail.com>
2023-01-07 22:36:00 +0530

    todo

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -91,13 +91,7 @@ describe("Todo test suite", () => {
       _csrf: csrfToken,
     }); 
 
-    const groupedTodosResponse = await agent
-      .get("/todos")
-      .set("Accept", "application/json");
-    const parsedGroupedResponse = JSON.parse(groupedTodosResponse.text);
-    const dueTodayCount = parsedGroupedResponse.duetodaytodos.length;
-    const latestTodo = parsedGroupedResponse.duetodaytodos[dueTodayCount - 1];
-
+    
     res = await agent.get("/todos");
     csrfToken = extractCsrfToken(res);
 
