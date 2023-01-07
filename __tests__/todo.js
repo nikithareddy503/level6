4a8fcdb06804d872184f5c553ff895155bfe25d2
nikithareddy503 <nikithareddychinni@gmail.com>
2023-01-07 22:33:25 +0530

    todo

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -59,7 +59,17 @@ describe("Todo test suite", () => {
     expect(res.statusCode).toBe(302);
   });
 
-  
+  test("Creates a todo", async () => {
+    const agent = request.agent(server);
+    await login(agent, "user.a@test.com", "123456");
+    const res = await agent.get("/todos");
+    const csrfToken = extractCsrfToken(res);
+    const response = await agent.post("/todos").send({
+      title: "Buy milk",
+      dueDate: new Date().toISOString(),
+      completed: false,
+      _csrf: csrfToken,
+    });
     expect(response.statusCode).toBe(302);
   });
 
