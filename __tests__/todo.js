18a8e864df1bec9b23f7805ce1c46f390fe4018c
nikithareddy503 <nikithareddychinni@gmail.com>
2023-01-07 22:37:56 +0530

    todo

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -113,7 +113,11 @@ describe("Todo test suite", () => {
         : expect(parsedUpdateResponse.completed).toBe(false);
     }); 
 
+    test("userA cannot update userB's todo", async () => {
+      const agent = request.agent(server);
   
+      let x1 = await agent.get("/signup");
+      let csrfToken = extractCsrfToken(x1);
 
     await agent.post("/users").send({
       firstName: "Test",
